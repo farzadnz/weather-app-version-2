792c7143ca727a9e92da90dd5d4504f480366f60
farzadnz <farzad2000nz@yahoo.co.nz>
2024-02-20 23:02:37 +1300

    correct the package.json and vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/react-weather-app-version-2/"
+  base: "/weather-app-version-2/"
 });
